8a6983f1218ce60e6dd91d2b82e0e5b761f74e14
Stephen Jang <mail@stephenjang.com>
2024-06-07 01:56:41 +0800

    Grouping component load time and Suspense

--- homework/app/dashboard/(overview)/page.tsx
+++ homework/app/dashboard/(overview)/page.tsx
@@ -2,12 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Suspense } from 'react';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
 
   return (
     <main>
@@ -23,11 +24,19 @@ export default async function Page() {
           value={numberOfCustomers}
           type="customers"
         /> */}
-        <CardWrapper/>
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper/>
+        </Suspense>        
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        {/* <RevenueChart revenue={revenue}  /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices/>
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
